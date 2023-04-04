a8a0c3ca2189206b6161932f31bd2cb847025c49
SiméonTambu <simeontambu@gmail.com>
2023-04-05 00:16:13 +0100

    Modification one

--- src/Router.php
+++ src/Router.php
@@ -1,16 +1,14 @@
 <?php
 namespace APP;
 class Router{
-    //Proprieté privé de 
-    // variable de type chaine de caractère
+    
     private $viewPath;
-    //Variable de type ALtorouter
     private $router;
     public function __construct(string $viewPath){
         $this->viewPath= $viewPath;
         $this->router= new \AltoRouter();
     }
-    //Création de notre méthode pour recuperer le chemin du fichier a afficher
+    //Creation of method to retrieve the path of the file to display
     public function get(string $url, string $view, ?string $name=null):self {
             $this->router->map('GET', $url, $view, $name);
             return $this;

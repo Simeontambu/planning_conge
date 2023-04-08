13a6897a09241acb2ec7ab42baa050a540069cac
SiméonTambu <simeontambu@gmail.com>
2023-04-08 11:40:52 +0100

    edit auth

--- src/Connexion.php
+++ src/Connexion.php
@@ -1,23 +1,21 @@
 <?php
-namespace App; 
+namespace App;
+
 // connection to bdd
-class Connexion{
-    private $DB_SERVER ="localhost";
-    private $DB_USERNAME ="root";
-    private $DB_PASSWORD= "";
-    private $DB_NAME="conge_agent";
+class Connexion
+{
+    private $DB_SERVER = "localhost";
+    private $DB_USERNAME = "root";
+    private $DB_PASSWORD = "";
+    private $DB_NAME = "conge_agent";
     public $conn;
 
-    public function getConnection(){
+    public function getConnection()
+    {
         $this->conn = null;
-        try{
-            $this->conn = mysqli_connect($this->DB_SERVER, $this->DB_USERNAME, $this->DB_PASSWORD, $this->DB_NAME);
-            return $this->conn;
-        }catch(Exception $e){
-            echo "erreur de connexion:", $e.getMessage();
-        }
-                       
+
+        $this->conn = mysqli_connect($this->DB_SERVER, $this->DB_USERNAME, $this->DB_PASSWORD, $this->DB_NAME);
+        return $this->conn;
+
     }
 }
- 
-?>
--- src/view/auth/auth.php
+++ src/view/auth/auth.php
@@ -1,43 +1,61 @@
 <?php
 
-
 use App\Connexion;
 
-
 $connexion = new Connexion();
-$database=$connexion->getConnection();
+$database = $connexion->getConnection();
 
-if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['pass'])){
-	// retrieve name user and delete \ add for form
-	$username = stripslashes($_REQUEST['username']);
-	$username = mysqli_real_escape_string($database,$username); 
-	// retrieve email and delete \ add for form
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($database, $email);
-	// retrieve password and delete \ add for form
-	$password = stripslashes($_REQUEST['pass']);
-	$password = mysqli_real_escape_string($database, $password);
-	
-	
-	
-	$query = "INSERT into `users` (id,username, email, pass)
-				VALUES (NULL,'$username', '$email', '".hash('sha256', $password)."')";
-	$res = mysqli_query($database, $query);
+/**
+ * Input field verification and data recovery
+ * Remove the backslashes added by the form
+ */
+if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['pass'])) {
+    $username = stripslashes($_REQUEST['username']);
+    $username = mysqli_real_escape_string($database, $username);
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($database, $email);
 
-    if($res){
-       echo "<div class='sucess'>
-             <h3>Vous êtes inscrit avec succès.</h3>
-             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
-			 </div>";
+    $password = stripslashes($_REQUEST['pass']);
+    $password = mysqli_real_escape_string($database, $password);
+
+    /**
+     * query to check if the user exists
+     */
+
+    $select = "SELECT * FROM users WHERE username = '" . $username . "'";
+    $res = mysqli_query($database, $select);
+    $info = array();
+    if (mysqli_num_rows($res)) {
+
+        $info[0] = 'Ce nom d\'utilisateur existe.';
+    } else {
+        /**
+         * insert data
+         */
+        $query = "INSERT into `users` (id,username, email, pass)
+                VALUES (NULL,'$username', '$email', '" . hash('sha256', $password) . "')";
+        $res = mysqli_query($database, $query);
+        if ($res) {
+            $info[1] = 'Vous êtes inscrit avec succès.';
+        }
     }
 }
+
 ?>
 
 <h1>S'inscrire</h1>
-<form action="" methode="post">
+<?php if (!empty($info)): {}?>
+				<div class="alert alert-danger">
+				<?php foreach ($info as $infos): ?>
+				<h3><?=$infos;?></h3>
+				<?php endforeach;?>
+</div>
+<?php endif;?>
+<form action="" methode="POST">
 <input type="text" placeholder="Nom d'utilisateur" name="username" required>
 <input type="email" placeholder="Adresse email" name="email" required>
-<input type="text" placeholder="Mot de passe" name="pass" required>
+<input type="text" placeholder="Mot de passe" name="pass"required>
 <input type="submit" name="submit" value="S'inscrire" class="box-button" />
 <p class="box-register">Déjà inscrit? <a href="/">Connectez-vous ici</a></p>
+
 </form>

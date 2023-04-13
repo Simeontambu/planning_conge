7febca49aa73acf03222a5a7de1faae02458b8ce
SiméonTambu <simeontambu@gmail.com>
2023-04-13 23:43:58 +0100

    leave modification

--- src/view/admin/conge.php
+++ src/view/admin/conge.php
@@ -1,36 +1,28 @@
-<form action="" method="POST" class="container w-50">
-<h1 class="text-center p-3 inscrite">Demande de congé</h1>
+<form action="" method="POST" class="">
+<h1 class="">Demande de congé</h1>
     <div class="form-group">
         <label id="inscrit" for="">Code de congé</label>
-        <input type="text" name="nom" class="form-control ">
+        <input type="text" name="nom" class="">
     </div>
-    <div class="form-group">
-        <label id="inscrit" for="">Code duré</label>
-        <select name="objet" id="" class="form-control">
+    <div class="">
+        <label id="" for="">Code duré</label>
+        <select name="objet" id="" class="">
             <option value=""></option>
-            <option value="cl">Conception des logiciels</option>
-            <option value="rt">Redaction Tfc</option>
-            <option value="ce">Cours d'encadrement</option>
+            <option value=""></option>
+
         </select>
     </div>
-    <div class="form-group">
+    <div class="">
         <label id="inscrit" for="">Code motif</label>
-        <select name="objet" id="" class="form-control">
+        <select name="objet" id="" class="">
             <option value=""></option>
-            <option value="cl">Conception des logiciels</option>
-            <option value="rt">Redaction Tfc</option>
-            <option value="ce">Cours d'encadrement</option>
         </select>
     </div>
-    <div class="form-group">
-        <label id="inscrit" for="">Matricule agent</label>
-        <select name="objet" id="" class="form-control">
+    <div class="">
+        <label id="inscrit" for="">Matricul">
             <option value=""></option>
-            <option value="cl">Conception des logiciels</option>
-            <option value="rt">Redaction Tfc</option>
-            <option value="ce">Cours d'encadrement</option>
         </select>
     </div>
-    <button type="submit" class="btn btn-primary bg-dark w-50 " id="bg-dark">Ajouté</button>
+    <button type="submit" class="" id="bg-dark">Ajouté</button>
 
 </form>
